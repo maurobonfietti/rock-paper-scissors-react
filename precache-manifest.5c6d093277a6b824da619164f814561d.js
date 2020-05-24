732c5fa70f0dbb0133bdac7737b5606007c2a4e5
Mauro Bonfietti <mauro.bonfietti@gmail.com>
2020-05-24 16:42:36 -0300

    Updates

---
+++ precache-manifest.5c6d093277a6b824da619164f814561d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0c26f668459401a5a184053f85b9fb74",
+    "url": "/rock-paper-scissors-react/index.html"
+  },
+  {
+    "revision": "5bb5d8d9b994de587a44",
+    "url": "/rock-paper-scissors-react/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "6d6ecb9e2f1cbb9b43de",
+    "url": "/rock-paper-scissors-react/static/js/2.a0856d11.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/rock-paper-scissors-react/static/js/2.a0856d11.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5bb5d8d9b994de587a44",
+    "url": "/rock-paper-scissors-react/static/js/main.50490b65.chunk.js"
+  },
+  {
+    "revision": "48723170453034b7face",
+    "url": "/rock-paper-scissors-react/static/js/runtime-main.bbb6e31e.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/rock-paper-scissors-react/precache-manifest.457580b9a131ef83a5fa7132bb7f2c43.js"
+  "/rock-paper-scissors-react/precache-manifest.5c6d093277a6b824da619164f814561d.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/rock-paper-scissors-react/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
